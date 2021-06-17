03deeed102b1cfd291f044ecfbbd81e28c556018
Durga Nayak <durganayak55@gmail.com>
2021-06-17 10:06:59 +0530

    modified step definition file

--- src/test/java/stepDefinitions/stepDefinitions.java
+++ src/test/java/stepDefinitions/stepDefinitions.java
@@ -26,7 +26,7 @@ public class stepDefinitions {
 		@Given("^user is on login page$")
 	    public void user_is_on_login_page() throws Throwable {
 			System.out.println("printing inside GIVEN");
-			System.out.println("printing again");
+			System.out.println("printing inside again");
 	        //throw new PendingException();
 				
 	    }
